cd8996c349d466186549ee16fe409cc69afd44ee
Sunaada Hebbar <shebbarm@asu.edu>
2021-12-01 00:21:19 -0700

    Updated script search for unreserved seats

--- src/main.py
+++ src/main.py
@@ -1,19 +1,14 @@
-from time import sleep
-
 import requests
 from plyer import notification
 from datetime import datetime
 
 class_url_base = "https://eadvs-cscc-catalog-api.apps.asu.edu/catalog-microservices/api/v1/search/classes?"
-reservation_url_base = "https://eadvs-cscc-catalog-api.apps.asu.edu/catalog-microservices/api/v1/search/reservedseatsadditionalinfo?"
 
 required_courses = [
     'Knowledge Representation',
-    'Software Requirements',
-    'Statistical Learning Theory',
-    'Modern Temporal Learning',
-    'AI Safety',
-    'Secure Microkernel',
+    'Mobile Computing',
+    'Semantic Web Mining',
+    'Data Visualization',
 ]
 
 class_parameters = {
@@ -22,19 +17,10 @@ class_parameters = {
     'searchType': 'all',
     'term': '2221',
     'subject': 'CSE',
-    'campus': 'TEMPE',
     'honors': 'F',
     'promod': 'F',
-    'level': 'grad'
-}
-
-reservation_parameters = {
-    'term': '2221',
-    'sessionCode': 'C',
-    'classType': 'E',
-    'acadCareer': 'GRAD',
-    'crseOfferNbr': '1',
-    'classSection': '1001'
+    'level': 'grad',
+    'campus': 'TEMPE',
 }
 
 email_ids = ['sunaada.hebbar@asu.edu']
@@ -65,36 +51,26 @@ def combine_params(given_parameters, new_parameters):
     return params
 
 
-def get_course_identifiers(data):
-    class_number = -1
-    course_id = -1
+def get_available_class_info(data):
     classes = []
+    class_details = {}
     if 'classes' in data:
         classes = data['classes']
-    for clas in classes:
-        if 'CLAS' in clas:
-            class_data = clas['CLAS']
-            if 'CLASSNBR' in class_data:
-                class_number = class_data['CLASSNBR']
-            if 'CRSEID' in class_data:
-                course_id = class_data['CRSEID']
-    return class_number, course_id
+    if len(classes) != 0:
+        for clas in classes:
+            if 'CLAS' in clas:
+                class_data = clas['CLAS']
+                count = int(class_data["ENRLCAP"]) - int(class_data["ENRLTOT"])
+                class_number = class_data["CLASSNBR"]
+                class_details[class_number] = count
+    return class_details
 
 
-def get_available_count(data):
-    for group in data:
-        if 'descr' in group:
-            if group['descr'] == "RC Master\'s Engin 1-3 courses":
-                count = group['enrlCap'] - group['enrlTot']
-                return count
-    return -1
-
-
-def notify(course, available_seats):
+def notify(course, course_number, available_seats):
     title = "Course Availability"
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    message = f"{available_seats} seats opened up at {current_time} for {course} course!! Hurry up!!"
+    message = f"{available_seats} seats opened up at {current_time} for {course} : {course_number} course!! Hurry up!!"
     notification.notify(
         title=title,
         message=message,
@@ -115,32 +91,26 @@ def get_course_availability(courses):
                 'keywords': course,
             })
         data = get_json_data(params, class_url_base)
-        class_number, course_id = get_course_identifiers(data)
-        if class_number == -1 or course_id == -1:
-            print(f"Course {course} Not Found")
+        info = get_available_class_info(data)
+        if len(info.items()) != 0:
+            available_courses_info[course] = info
         else:
-            params = combine_params(
-                reservation_parameters,
-                {
-                    'classNbr': class_number,
-                    'crseId': course_id,
-                })
-            data = get_json_data(params, reservation_url_base)
-            available_courses_info[course] = get_available_count(data)
+            print(f"Reservation NOT available for the course {course}")
     return available_courses_info
 
 
 def main():
     available_courses_info = get_course_availability(required_courses)
-    for course, available_seats in available_courses_info.items():
-        if available_seats > 0:
-            print(f"Course {course} has {available_seats} available seats")
-            notify(course, available_seats)
-        if available_seats == 0:
-            print(f"Course {course} has NO seats available")
-            # notify(course, available_seats)
-        else:
-            print(f"Reservation NOT available for the course {course}")
+    for course, course_info in available_courses_info.items():
+        course_numbers = course_info.keys()
+        for course_number in course_numbers:
+            available_seats = course_info[course_number]
+            if available_seats > 0:
+                print(f"Course {course} : {course_number} has {available_seats} available seats")
+                notify(course, course_number, available_seats)
+            if available_seats == 0:
+                print(f"Course {course} : {course_number} has NO seats available")
+                # notify(course, available_seats)
 
 
 if __name__ == "__main__":
